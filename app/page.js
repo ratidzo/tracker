d3558ba10d348ac29cb4fade5a5447fc3c728bb3
ratidzo <ratygore@gmail.com>
2023-10-30 10:49:55 +0200

    dev: setup form

--- app/page.js
+++ app/page.js
@@ -5,6 +5,14 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
         <h1 className='text-4xl p-4 text-center'>Expense Tracker</h1>
+
+        <div className='bg-slate-800 p-4 rounded-lg'>
+          <form className='grid grid-cols-6 items-center text-black'>
+            <input type='text' placeholder='Enter item'/>
+            <input type='text' placeholder='Enter $'/>
+            <button type='submit'>+</button>
+          </form>
+        </div>
       </div>
     </main>
   )
